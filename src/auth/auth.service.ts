3e3fa29491102ffa272e6e9b376bd8d64662034a
anik_ad78 <anikinworkplace@gmail.com>
2023-03-11 18:43:35 +0100

    add tokens add jwt strategy

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -8,29 +8,24 @@ import { ModelType } from '@typegoose/typegoose/lib/types'
 import { UserModel } from 'src/user/user.model'
 import { AuthDto } from './dto/auth.dto'
 import { compare, genSalt, hash } from 'bcryptjs'
+import { JwtService } from '@nestjs/jwt'
 
 @Injectable()
 export class AuthService {
 	constructor(
-		@InjectModel(UserModel) private readonly UserModel: ModelType<UserModel>
+		@InjectModel(UserModel) private readonly UserModel: ModelType<UserModel>,
+		private readonly JwtService: JwtService
 	) {}
 
 	async login(dto: AuthDto) {
-		const { email, password } = dto
+		const user = await this.validateUser(dto)
 
-		const user = await this.UserModel.findOne({ email })
+		const tokens = await this.issueTokenPair(String(user._id))
 
-		if (!user) {
-			throw new UnauthorizedException('User not found')
+		return {
+			user: this.returnUserFields(user),
+			...tokens,
 		}
-
-		const isValidPassword = await compare(password, user.password)
-
-		if (!isValidPassword) {
-			throw new UnauthorizedException('Invalid password')
-		}
-
-		return user
 	}
 
 	async register(dto: AuthDto) {
@@ -48,6 +43,54 @@ export class AuthService {
 			password: await hash(password, salt),
 		})
 
-		return newUser.save()
+		const tokens = await this.issueTokenPair(String(newUser._id))
+
+		return {
+			user: this.returnUserFields(newUser),
+			...tokens,
+		}
+	}
+
+	async issueTokenPair(userId: string) {
+		const data = { _id: userId }
+
+		const refreshToken = await this.JwtService.signAsync(data, {
+			expiresIn: '15d',
+		})
+
+		const accessToken = await this.JwtService.signAsync(data, {
+			expiresIn: '1h',
+		})
+
+		return {
+			refreshToken,
+			accessToken,
+		}
+	}
+
+	async validateUser(dto: AuthDto) {
+		const { email, password } = dto
+		const user = await this.UserModel.findOne({ email })
+
+		if (!user) {
+			throw new UnauthorizedException('User not found')
+		}
+
+		const isValidPassword = await compare(password, user.password)
+
+		if (!isValidPassword) {
+			throw new UnauthorizedException('Invalid password')
+		}
+
+		return user
+	}
+
+	returnUserFields(user: UserModel) {
+		const { _id, email, isAdmin } = user
+		return {
+			_id,
+			email,
+			isAdmin,
+		}
 	}
 }
