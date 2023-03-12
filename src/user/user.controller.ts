cf7250f3a9d398226d0b37bae18dff7a05cd784a
anik_ad78 <anikinworkplace@gmail.com>
2023-03-12 13:38:06 +0100

    update user profile add IdValidationPipe

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,7 +1,18 @@
-import { Controller, Get } from '@nestjs/common'
+import {
+	Body,
+	Controller,
+	Get,
+	HttpCode,
+	Param,
+	Put,
+	UsePipes,
+	ValidationPipe,
+} from '@nestjs/common'
 import { UserService } from './user.service'
 import { Auth } from '../auth/decorators/auth.decorator'
 import { User } from './decorators/user.decoratoe'
+import { UpdateUserDto } from './dto/updateUser.dto'
+import { IdValidationPipes } from '../pipes/id.validation.pipe'
 
 @Controller('users')
 export class UserController {
@@ -11,5 +22,24 @@ export class UserController {
 	@Auth()
 	async getProfile(@User('_id') _id: string) {
 		return this.userService.byId(_id)
+	}
+
+	@UsePipes(new ValidationPipe())
+	@Put('profile')
+	@HttpCode(200)
+	@Auth()
+	async updateProfile(@User('_id') _id: string, @Body() dto: UpdateUserDto) {
+		return this.userService.updateProfile(_id, dto)
+	}
+
+	@UsePipes(new ValidationPipe())
+	@Put(':id')
+	@HttpCode(200)
+	@Auth('admin')
+	async updateUser(
+		@Param('id', IdValidationPipes) id: string,
+		@Body() dto: UpdateUserDto
+	) {
+		return this.userService.updateProfile(id, dto)
 	}
 }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -2,6 +2,8 @@ import { Injectable, NotFoundException } from '@nestjs/common'
 import { ModelType } from '@typegoose/typegoose/lib/types'
 import { UserModel } from './user.model'
 import { InjectModel } from 'nestjs-typegoose'
+import { UpdateUserDto } from './dto/updateUser.dto'
+import { genSalt, hash } from 'bcryptjs'
 
 @Injectable()
 export class UserService {
@@ -17,5 +19,28 @@ export class UserService {
 		}
 
 		return user
+	}
+
+	async updateProfile(_id: string, dto: UpdateUserDto) {
+		const user = await this.byId(_id)
+		const isSameUser = await this.userModel.findOne({ email: dto.email })
+
+		if (isSameUser && String(_id) !== String(isSameUser._id)) {
+			throw new NotFoundException('Email already exist!')
+		}
+
+		if (dto.password) {
+			const salt = await genSalt(10)
+			user.password = await hash(dto.password, salt)
+		}
+
+		user.email = dto.email
+
+		if (dto.isAdmin || dto.isAdmin === false) {
+			user.isAdmin = dto.isAdmin
+		}
+
+		await user.save()
+		return
 	}
 }
